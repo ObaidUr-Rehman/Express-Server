56ed4f39bf4a4b00190f4d0e16c32f89bbfeee4f
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-11-23 14:05:01 +0200

    my first server

---
+++ server.mjs
@@ -0,0 +1,15 @@
+console.log("My first server")
+
+import express from "express";
+const app = express()
+const port =process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log("resquest get" + req.ip)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
