0806be90cc1791106bd930fb5a27eb30b5b5eadf
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-11-25 22:32:58 +0500

    new build

--- server.mjs
+++ server.mjs
@@ -1,14 +1,18 @@
-console.log("My first server")
-
 import express from "express";
+import path, { dirname } from 'path';
 const app = express()
 const port =process.env.PORT || 3000
 
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
   res.send('Hello World!')
   console.log("resquest get" + req.ip)
 })
+
+const __dirname=path.resolve();
+app.use('/',express.static(path.join(__dirname,'/web/build')))
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
