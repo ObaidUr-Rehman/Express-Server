00b016b21a34069e7f7c2b294a550a7df4c7a5d3
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-11-30 22:41:42 +0500

    done

--- server.mjs
+++ server.mjs
@@ -1,15 +1,37 @@
-console.log("My first server")
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
 
-import express from "express";
 const app = express()
-const port =process.env.PORT || 3000
+const port = process.env.PORT || 5001;
 
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log("resquest get" + req.ip)
+app.get('/abc', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+app.get('/weather', (req, res) => {
+    console.log("request ip: ", req.ip);
+    
+    res.send({
+        temp: 30,
+        min: 26,
+        max: 31,
+        humidity: 72,
+        serverTime: new Date().toString()
+    });
+})
+app.get('/time', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
 })
 
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
---
+++ web/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react"
+import axios from "axios";
+
+
+let baseUrl = ``;
+if (window.location.href.split(":")[0] === "http") {
+    baseUrl = `http://localhost:5001`;
+}
+
+
+function App() {
+
+    const [weatherData, setWeatherData] = useState(null)
+    const [cityName, setCityName] = useState("")
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log("I am click handler")
+        axios.get(`${baseUrl}/weather`)
+            .then(response => {
+                console.log("response: ", response.data);
+
+                setWeatherData(response.data);
+            })
+            .catch(err => {
+                console.log("error: ", err);
+            })
+    }
+
+    return (
+      <>
+      <div className="box">
+          <div className="inputData">
+              <input
+                  type="search" className="inputField"
+                  onChange={(event) => { setWeatherData (event.target.value)
+                  }} />
+                  <br />
+                  
+                  
+          </div>
+               <div>
+                  <button className="mybtn" onClick={submitHandler}>Get Weather</button>
+                  </div>
+      
+
+      { !weatherData ? (
+          <p className='errorMsg'>No Data Found</p>
+      ) : ( 
+
+          <div>
+          <div className="info">
+
+              {/* <h1 className="location">
+              {weatherData}
+              </h1> */}
+
+              <h2 lassName="temp">
+                 Current Temp: {weatherData.temp}°C
+              </h2>
+
+              <h3 className="tempmin_max"> 
+              Min: {weatherData.min}°C | Max: {weatherData.max}°C
+              </h3>
+
+          </div>
+
+          <div className="wave -one"></div>
+          <div className="wave -two"></div>
+          <div className="wave -three"></div>
+          </div>
+
+      )}
+
+          
+      </div>
+  </>
+    );
+}
+
+export default App;
