8894805585e9237ef0d9dc38f9c1795c5668a1b4
Obaid Ur Rehman <96888672+ObaidUr-Rehman@users.noreply.github.com>
2022-12-12 22:39:30 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 
+
 const app = express()
 const port = process.env.PORT || 5001;
 
@@ -27,10 +28,10 @@ app.get('/time', (req, res) => {
     res.send('Hello World! ' + new Date().toString());
 })
 
+
 const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname, './web/build')))
 app.use('*', express.static(path.join(__dirname, './web/build')))
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
